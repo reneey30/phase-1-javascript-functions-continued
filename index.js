e73ba247b4ce353c0bbab0e9594367051af4a068
Irene Njeru <irenenjeru8@gmail.com>
2022-04-08 13:15:29 +0300

    completed task

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(toDo ="roller-skate") {
+    
+    return "This Saturday, I want to " + toDo + "!";
+}
+ 
+function mondayWork(task = "go to the office") {
+    return "This Monday, I will " + task + "."
+}
+
+function wrapAdjective(highlight) {
+    const innerFunction = function(adjective){
+        return "You are " + highlight + adjective + highlight + "!";
+    }
+    return innerFunction;
+}
